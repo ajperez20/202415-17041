f862729da9f28726d10ce7fd1db6885dcf0a3882
Alejandro Senior <aesenior.20@est.ucab.edu.ve>
2023-12-06 16:15:54 -0400

    Created login

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    let year = 2023;
+    let now = new Date().getSeconds();
+</script>
+
+<div>
+    <h1>Login</h1>
+    <p>Welcome to Khatzoo!</p>
+    <h2>Are you registered?</h2>
+    <p>If not, please go to the <a href="/signup">signup page</a>.</p>
+</div>
+
+<style>
+    div{
+        background: #000;
+        color: #f0f;
+    }
+
+    h1 {
+        font-size: 1rem;
+    }
+</style>
